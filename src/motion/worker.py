c12bf312e6c05444bf480fd4c3aa448e27f1d9cb
Borgstrom ♕ <evan@borgstrom.ca>
2017-05-23 09:55:00 -0700

    Add a worker process for the consumer (#1)

--- src/motion/worker.py
+++ src/motion/worker.py
@@ -3,58 +3,89 @@ import logging
 import Queue
 import multiprocessing
 import signal
+import sys
 
 log = logging.getLogger(__name__)
 
 
-class MotionWorker(object):
-    def __init__(self, queue, responders):
-        self.queue = queue
-        self.responders = responders
+class MotionProcess(object):
+    """Base class for forked workers via multiprocessing.Process"""
+    def __init__(self):
         self.process = None
         self.alive = True
 
         atexit.register(self.shutdown)
-        self.process = multiprocessing.Process(target=self.run)
+        self.process = multiprocessing.Process(target=self._run)
         self.process.start()
 
     def shutdown(self):
         if self.process:
-            log.info("Worker shutting down")
+            log.info("%s shutting down", self.__class__.__name__)
             self.process.terminate()
             self.process.join()
             self.process = None
 
-    def signal_handler(self, signum, frame):
+    def _signal_handler(self, signum, frame):
         log.info("Caught signal %s", signum)
         self.alive = False
 
-    def run(self):
-        signal.signal(signal.SIGTERM, self.signal_handler)
+    def _run(self):
+        signal.signal(signal.SIGTERM, self._signal_handler)
 
-        log.info("Worker starting")
+        log.info("%s starting", self.__class__.__name__)
 
         while self.alive:
             try:
-                event_name, payload = self.queue.get(block=True, timeout=0.1)
-            except Queue.Empty:
-                continue
-            except (SystemExit, KeyboardInterrupt):
-                log.error("Exiting via interrupt")
+                self.work()
+            except Exception:
+                log.exception("Unhandled exception in base MotionProcess")
                 self.alive = False
-                break
-            except Exception:
-                log.exception("Failed to get event & payload from queue")
-                continue
+                return sys.exit(1)
 
-            responder = self.responders[event_name]
+    def work(self):
+        raise NotImplementedError
 
-            try:
-                result = responder(payload)
-            except Exception:
-                log.exception("Unhandled exception while processing payload for event %s", event_name)
-                continue
 
-            log.debug("Processed event %s with payload %s, got result %s", event_name, payload, result)
+class MotionConsumer(MotionProcess):
+    def __init__(self, app):
+        self.app = app
+        super(MotionConsumer, self).__init__()
 
-            # XXX TODO: add result storage?
+    def work(self):
+        self.app.consume()
+
+        # we should never reach this
+        log.error("Failed to consume from Kinesis!")
+        sys.exit(2)
+
+
+class MotionWorker(MotionProcess):
+    def __init__(self, queue, responders):
+        self.queue = queue
+        self.responders = responders
+        super(MotionWorker, self).__init__()
+
+    def work(self):
+        try:
+            event_name, payload = self.queue.get(block=True, timeout=0.1)
+        except Queue.Empty:
+            return
+        except (SystemExit, KeyboardInterrupt):
+            log.error("Exiting via interrupt")
+            self.alive = False
+            return
+        except Exception:
+            log.exception("Failed to get event & payload from queue")
+            return
+
+        responder = self.responders[event_name]
+
+        try:
+            result = responder(payload)
+        except Exception:
+            log.exception("Unhandled exception while processing payload for event %s", event_name)
+            return
+
+        log.debug("Processed event %s with payload %s, got result %s", event_name, payload, result)
+
+        # XXX TODO: add result storage?
