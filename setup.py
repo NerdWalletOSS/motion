613c840ba803b7e21b251e3ea3cc2b9df4caf35b
Evan Borgstrom <eborgstrom@nerdwallet.com>
2019-10-20 10:16:27 +0800

    Bump min version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('VERSION') as version_fd:
 
 install_requires = [
     'click>=6.6,<7.0',
-    'kinesis-python>=0.2.0,<0.9',
+    'kinesis-python>=0.2.1,<0.9',
     'offspring>=0.1.1,<1.0',
     'six>=1.12.0,<2.0'
 ]
